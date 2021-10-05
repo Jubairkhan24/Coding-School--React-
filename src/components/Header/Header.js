934f15ad3c7f4c05279b230e90182822f6e6340c
Jubair Istiaq Khan <superanonto001@gmail.com>
2021-10-05 17:12:49 +0600

    last commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,13 +6,6 @@ import './Header.css';
 const Header = () => {
     return (
         <div className="header-nav">
-            {/* <h1>Bidyalay Dom Com</h1> */}
-            {/* <nav className=" nav-text-style">
-                <NavLink to="/home">Home</NavLink>
-                <NavLink to="/about us">About Us</NavLink>
-                <NavLink to="/services">Services</NavLink>
-                <NavLink to="/contact us">Contact Us</NavLink>
-            </nav> */}
             <Navbar variant="">
                 <Container className="nav-style">
                     <Navbar.Brand className="text-light">Coding Bidyalaya </Navbar.Brand>
